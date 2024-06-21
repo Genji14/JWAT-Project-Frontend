e21b72e189b0582caa2015018630c83ed7da89ab
Genji-kun <2051052097phat@ou.edu.vn>
2024-06-21 14:45:54 +0700

    [Responsive] - Update Responsive Project Detail Page

--- src/components/pages/Projects/ProjectDetail/ProjectContainer/index.tsx
+++ src/components/pages/Projects/ProjectDetail/ProjectContainer/index.tsx
@@ -10,8 +10,8 @@ const ProjectContainer: React.FC = () => {
 
     return (
         <>
-            <div className="grid grid-cols-12 gap-4 ">
-                <div className="col-start-2 col-span-7 w-full flex flex-col gap-2 rounded shadow-md">
+            <div className="grid grid-cols-3 lg:grid-cols-12 gap-4 ">
+                <div className="col-span-3 lg:col-start-2 lg:col-span-7 w-full flex flex-col gap-2 rounded shadow-md">
                     <div className="flex gap-2 h-24 bg-background">
                         <div className='h-full'>
                         </div>
@@ -81,7 +81,7 @@ const ProjectContainer: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col-start-10 col-span-3 w-full relative">
+                <div className="lg:col-start-10 col-span-3 w-full relative hidden lg:block">
                     <div className="sticky top-40">
                         <ProjectKnowledge knowledge={"hehe"} />
                     </div>
