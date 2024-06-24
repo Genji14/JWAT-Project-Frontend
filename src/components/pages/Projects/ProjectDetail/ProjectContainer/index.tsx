97fab977b65f580f20a8bd31c27b363ddf7b987e
Genji-kun <2051052097phat@ou.edu.vn>
2024-06-24 16:04:38 +0700

    [Update] Clean code

--- src/components/pages/Projects/ProjectDetail/ProjectContainer/index.tsx
+++ src/components/pages/Projects/ProjectDetail/ProjectContainer/index.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ProjectKnowledge from '../ProjectKnowledge';
-import { useProjectDetailContext } from '@/lib/contexts/ProjectDetailProject';
 import BlogList from '../BlogList';
 
-
-
 const ProjectContainer: React.FC = () => {
-
-    const { project } = useProjectDetailContext();
 
     return (
         <>
             <div className="grid grid-cols-3 lg:grid-cols-12 gap-4 ">
-                <div className="col-span-3 lg:col-start-2 lg:col-span-7 w-full flex flex-col gap-2 rounded shadow-md">
+                <div className="col-span-3 lg:col-start-2 lg:col-span-7 w-full flex flex-col gap-2">
                     <BlogList />
                 </div>
                 <div className="lg:col-start-10 col-span-3 w-full relative hidden lg:block">
