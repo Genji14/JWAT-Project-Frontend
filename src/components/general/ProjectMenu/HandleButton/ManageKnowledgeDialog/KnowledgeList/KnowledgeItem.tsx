e23879c0445188a4768b6cd271b531a4a3b831e5
Rocky-cyberclop <rockyoperation@gmail.com>
2024-06-24 17:27:20 +0700

    [Knowledge features] Remove knowledge feature

--- src/components/general/ProjectMenu/HandleButton/ManageKnowledgeDialog/KnowledgeList/KnowledgeItem.tsx
+++ src/components/general/ProjectMenu/HandleButton/ManageKnowledgeDialog/KnowledgeList/KnowledgeItem.tsx
@@ -1,23 +1,28 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Button } from '@/components/ui/button'
 import { IKnowledge } from '@/types/interfaces/Knowledge'
 import { format } from 'date-fns'
 import Image from 'next/image'
 import React from 'react'
+import RemoveKnowledgeDialog from './RemoveKnowledgeDialog/RemoveKnowledgeDialog'
 
 const KnowledgeItem = ({ knowledge }: { knowledge: IKnowledge }) => {
     return (
-        <div className='w-full flex gap-2 items-center'>
-            <div className="w-12 h-12 rounded-md overflow-hidden">
-                <Image src={knowledge.media.url} alt={knowledge.name} height={500} width={500} className='aspect-square w-full object-cover' />
+        <div className='flex w-full items-center gap-2'>
+            <div className='h-12 w-12 overflow-hidden rounded-md'>
+                <Image
+                    src={knowledge.media.url}
+                    alt={knowledge.name}
+                    height={500}
+                    width={500}
+                    className='aspect-square w-full object-cover'
+                />
             </div>
             <div className='flex-auto'>
-                <h4 className="font-semibold">{knowledge.name}</h4>
-                <h5 className='text-xs text-muted-foreground' >Created at: {format(knowledge.createdAt, "PPP")} </h5>
+                <h4 className='font-semibold'>{knowledge.name}</h4>
+                <h5 className='text-xs text-muted-foreground'>
+                    Created at: {format(knowledge.createdAt, 'PPP')}{' '}
+                </h5>
             </div>
-            <Button variant={"destructive"} className='px-3 py-1.5'>
-                Delete
-            </Button>
+            <RemoveKnowledgeDialog knowledge={knowledge} />
         </div>
     )
 }
--- src/services/knowledge.service.ts
+++ src/services/knowledge.service.ts
@@ -1,21 +1,28 @@
-import { KNOWLEDGE_ENDPOINTS } from "@/lib/constants/EndPoints";
-import { AxiosResponse } from "axios";
-import { IKnowledge } from "@/types/interfaces/Knowledge";
-import API_INSTANCE from "@/lib/api";
+import { KNOWLEDGE_ENDPOINTS } from '@/lib/constants/EndPoints'
+import { AxiosResponse } from 'axios'
+import { IKnowledge } from '@/types/interfaces/Knowledge'
+import API_INSTANCE from '@/lib/api'
 
 class KnowledgeService {
-
-    getKnowledgeByProjectId = (projectId: number): Promise<AxiosResponse<IKnowledge[]>> => {
-        return API_INSTANCE.get(KNOWLEDGE_ENDPOINTS.GET_KNOWLEDGE_BY_PROJECT(projectId));
+    getKnowledgeByProjectId = (
+        projectId: number
+    ): Promise<AxiosResponse<IKnowledge[]>> => {
+        return API_INSTANCE.get(
+            KNOWLEDGE_ENDPOINTS.GET_KNOWLEDGE_BY_PROJECT(projectId)
+        )
     }
 
     createKnowledge = (form: FormData) => {
         return API_INSTANCE.post(KNOWLEDGE_ENDPOINTS.CREATE_KNOWLEDGE, form, {
             headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        });
+                'Content-Type': 'multipart/form-data',
+            },
+        })
+    }
+
+    deleteKnowledge = (id: number) => {
+        return API_INSTANCE.delete(KNOWLEDGE_ENDPOINTS.REMOVE_KNOWLEDGE(id))
     }
 }
 
-export const knowledgeService = new KnowledgeService();
+export const knowledgeService = new KnowledgeService()
