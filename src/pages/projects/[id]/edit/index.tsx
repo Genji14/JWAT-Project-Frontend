7a80dedadbbefa6dff6ae7304a1ba3492882b2a4
Rocky-cyberclop <rockyoperation@gmail.com>
2024-06-24 21:55:12 +0700

    [Redockerize frontend] Reconfig docker after fix refresh token bug

--- src/pages/projects/[id]/edit/index.tsx
+++ src/pages/projects/[id]/edit/index.tsx
@@ -1,6 +1,6 @@
 import ProjectDetailLayout from "@/components/layouts/ProjectDetail";
 import EditProjectContainer from "@/components/pages/Projects/ProjectDetail/EditProject/EditProject/EditProjectContainer";
-import { API_INSTANCE, authorizeSSR } from "@/lib/constants/ApiInstance";
+// import { API_INSTANCE, authorizeSSR } from "@/lib/constants/ApiInstance";
 import { PROJECT_ENDPOINTS } from "@/lib/constants/EndPoints";
 import { ProjectDetailProvider } from "@/lib/contexts/ProjectDetailProject";
 import { IProject } from "@/types/interfaces/Project";
@@ -24,15 +24,15 @@ const EditProjectPage = ({ project }: { project: IProject }) => {
 }
 
 export async function getServerSideProps({ req, params }: any) {
-    try {
-        const { id } = params;
-        authorizeSSR(req);
-        const res = await API_INSTANCE.get(PROJECT_ENDPOINTS.FIND_ONE(id));
-        return { props: { project: res.data } };
-    } catch (error) {
-        console.error(error);
-    }
-    return { props: { project: null } };
+    // try {
+    //     const { id } = params;
+    //     authorizeSSR(req);
+    //     const res = await API_INSTANCE.get(PROJECT_ENDPOINTS.FIND_ONE(id));
+    //     return { props: { project: res.data } };
+    // } catch (error) {
+    //     console.error(error);
+    // }
+    // return { props: { project: null } };
 
 }
 export default EditProjectPage;
