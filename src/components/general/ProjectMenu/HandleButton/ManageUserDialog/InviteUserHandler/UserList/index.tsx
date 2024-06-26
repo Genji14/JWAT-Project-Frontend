065f13f022fc4149a8c4c93cc1474f98ce8f0367
Rocky-cyberclop <rockyoperation@gmail.com>
2024-06-26 21:07:01 +0700

    [User management] Wokring on pagination when add users to project

--- src/components/general/ProjectMenu/HandleButton/ManageUserDialog/InviteUserHandler/UserList/index.tsx
+++ src/components/general/ProjectMenu/HandleButton/ManageUserDialog/InviteUserHandler/UserList/index.tsx
@@ -1,31 +1,60 @@
 import { ScrollArea } from '@/components/ui/scroll-area'
 import React from 'react'
-import { IUserNotInProject } from '@/types/interfaces/User';
-import InviteUserLoading from './UserLoading';
-import InviteUserItem from './UserItem';
-import { UserRoundXIcon } from 'lucide-react';
+import { IPaginationUserInfor } from '@/types/interfaces/User'
+import InviteUserLoading from './UserLoading'
+import InviteUserItem from './UserItem'
+import { UserRoundXIcon } from 'lucide-react'
+import {
+    Pagination,
+    PaginationContent,
+    PaginationItem,
+    PaginationNext,
+    PaginationPrevious,
+} from '@/components/ui/pagination'
 
-const InviteUserList = ({ data, isFetching }: { data: IUserNotInProject[] | undefined, isFetching: boolean }) => {
-
+const InviteUserList = ({
+    data,
+    isFetching,
+}: {
+    data: IPaginationUserInfor | undefined
+    isFetching: boolean
+}) => {
     return (
         <ScrollArea className='max-h-56'>
             <div className='flex flex-col gap-2'>
-                {
-                    !isFetching ? <>
-                        {
-                            !!data?.length ? data?.map((user) => {
-                                return <InviteUserItem key={user.id} data={user} />
-                            }) :
-                                <div className="my-3 text-muted-foreground  flex flex-col items-center justify-center">
-                                    <UserRoundXIcon />
-                                    <span className='text-sm text-center'>No user avaiable !!</span>
-                                </div>
-                        }
-                    </> : <InviteUserLoading />
-                }
+                {!isFetching ? (
+                    <>
+                        {data?.items.length ? (
+                            data?.items.map((user) => {
+                                return (
+                                    <InviteUserItem key={user.id} data={user} />
+                                )
+                            })
+                        ) : (
+                            <div className='my-3 flex  flex-col items-center justify-center text-muted-foreground'>
+                                <UserRoundXIcon />
+                                <span className='text-center text-sm'>
+                                    No user avaiable !!
+                                </span>
+                            </div>
+                        )}
+                        <Pagination>
+                            <PaginationContent>
+                                <PaginationItem>
+                                    <PaginationPrevious href='#' />
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationNext href='#' />
+                                </PaginationItem>
+                            </PaginationContent>
+                        </Pagination>
+                    </>
+                ) : (
+                    <InviteUserLoading />
+                )}
             </div>
         </ScrollArea>
     )
 }
 
-export default InviteUserList;
+export default InviteUserList
--- src/components/general/ProjectMenu/HandleButton/ManageUserDialog/InviteUserHandler/index.tsx
+++ src/components/general/ProjectMenu/HandleButton/ManageUserDialog/InviteUserHandler/index.tsx
@@ -1,35 +1,38 @@
 import React, { useState } from 'react'
-import { Button } from '@/components/ui/button';
-import { useStore } from '@/components/providers/StoreProvider';
-import { Input } from '@/components/ui/input';
-import { Search } from 'lucide-react';
-import { useDebounce } from '@/hooks/useDebounce';
-import { useSearchUserNotInProject } from '@/hooks/query/project.query';
-import InviteUserList from './UserList';
-import { DEBOUNCE_TIME } from '@/lib/constants/SettingSystem';
+import { Button } from '@/components/ui/button'
+import { useStore } from '@/components/providers/StoreProvider'
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
+import { useDebounce } from '@/hooks/useDebounce'
+import { useSearchUserNotInProject } from '@/hooks/query/project.query'
+import InviteUserList from './UserList'
+import { DEBOUNCE_TIME } from '@/lib/constants/SettingSystem'
 
 const InviteUserHandler = () => {
+    const toggleAdding = useStore((state) => state.toggleAdding)
+    const [query, setQuery] = useState<string>('')
 
-    const toggleAdding = useStore(state => state.toggleAdding);
-    const [query, setQuery] = useState<string>("");
+    const debounceQuery = useDebounce(query, DEBOUNCE_TIME)
 
-    const debounceQuery = useDebounce(query, DEBOUNCE_TIME);
+    const { isFetchingUser, userData } =
+        useSearchUserNotInProject(debounceQuery)
 
-    const { isFetchingUser, userData } = useSearchUserNotInProject(debounceQuery);
-
+    console.log(userData)
     return (
-        <div className="flex flex-col gap-2">
+        <div className='flex flex-col gap-2'>
             <div className='flex items-center justify-between gap-2'>
-                <h3 className="font-semibold text-lg">Invite member</h3>
-                <div className="flex gap-2 items-center">
+                <h3 className='text-lg font-semibold'>Invite member</h3>
+                <div className='flex items-center gap-2'>
                     <Input
-                        icon={<Search className='h-4 w-4 text-muted-foreground' />}
+                        icon={
+                            <Search className='h-4 w-4 text-muted-foreground' />
+                        }
                         value={query}
                         onChange={(evt) => setQuery(evt.target.value)}
                         placeholder='Search employee ID'
-                        className="w-72"
+                        className='w-72'
                     />
-                    <Button variant={"outline"} onClick={toggleAdding}>
+                    <Button variant={'outline'} onClick={toggleAdding}>
                         Back
                     </Button>
                 </div>
